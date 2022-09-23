7cd8311f9e958732e2c67416dbe10c00f0c9660a
Anuj Kumar Pandey <anujkumarpandey@Anujs-MacBook-Pro.local>
2022-09-23 21:09:11 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from './components/Navbar';
+import Profile from './pages/Profile';
+import Chat from './pages/Chat'
 
 function App() {
   return (
     <>
       <Router>
         <Navbar/>
-        {/* <Alert customAlert={customAlert}/> */}
-
+        
         <Routes>
           <Route path="/" element={<Home/>}></Route>
+          <Route path="/me" element={<Profile/>}></Route>
+          <Route path="/chat" element={<Chat/>}></Route>
         </Routes>
 
       </Router>
